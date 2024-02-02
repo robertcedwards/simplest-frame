4ea2ab46a6e38193c63fc85d396fa6c3d8839588
Robert C Edwards <139775+robertcedwards@users.noreply.github.com>
2024-02-01 20:49:25 -0600

    d

--- src/frames/count.js
+++ src/frames/count.js
@@ -1,7 +1,6 @@
 import mainLayout from '../layouts/main';
 import { getFramer, setFramer } from '../data/framer';
 import { getCount, incrementCount } from '../data/count';
-const username = data.result.user.username;
 
 export default {
     image: `https://api.crawlbase.com/screenshots?token=4HrVHd6RoiajGYYEJsx0QA&height=700&url=https://web3.bio/${lastFramerUsername}`,
