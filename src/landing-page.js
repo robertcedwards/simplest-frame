14e007c9979cc2b0856c73ce9e596ece1853b372
Robert C Edwards <139775+robertcedwards@users.noreply.github.com>
2024-02-01 18:40:33 -0600

    Update landing-page.js

--- src/landing-page.js
+++ src/landing-page.js
@@ -69,7 +69,7 @@ export default async (frameContent) => {
         <body>
             <h1>🔳 The Simplest Frame</h1>
             <figure>
-                <img width="600" src="https://api.crawlbase.com/screenshots?token=4HrVHd6RoiajGYYEJsx0QA&url=https://web3.bio/" + lastFramerUsername />
+                <img src="https://api.crawlbase.com/screenshots?token=4HrVHd6RoiajGYYEJsx0QA&url=https://web3.bio/ + ${lastFramerUsername}" />
             </figure>
         </body>
         </html>
