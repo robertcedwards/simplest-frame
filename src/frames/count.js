8d0aef2dc0757028e6f76aaf382feb3841c45d6a
Robert C Edwards <139775+robertcedwards@users.noreply.github.com>
2024-02-01 20:44:40 -0600

    link

--- src/frames/count.js
+++ src/frames/count.js
@@ -1,8 +1,7 @@
 const username = data.result.user.username;
-return username;
 
 export default {
-    image: (username) => `https://api.crawlbase.com/screenshots?token=4HrVHd6RoiajGYYEJsx0QA&height=700&url=https://web3.bio/${username}`,
+    image: `https://api.crawlbase.com/screenshots?token=4HrVHd6RoiajGYYEJsx0QA&height=700&url=https://web3.bio/${lastFramerUsername}`,
     buttons: [
         { 
             label: '⬅️ Back',
