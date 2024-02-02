c920a2fa6f435ebb7f05908ccccac4ec29b4b2bc
Robert C Edwards <139775+robertcedwards@users.noreply.github.com>
2024-02-01 20:51:09 -0600

    s

--- src/frames/count.js
+++ src/frames/count.js
@@ -1,9 +1,22 @@
 import mainLayout from '../layouts/main';
 import { getFramer, setFramer } from '../data/framer';
 import { getCount, incrementCount } from '../data/count';
+const getFramer = async() => {
+    const store = getStore('gameState');
+    const framerId = await store.get('framer');
 
+    const request = await fetch(`${wieldAPIBase}/user?fid=${framerId}`, {
+        method: "GET",
+        headers: { "API-KEY": wieldKey }
+    });
+
+    const body = await streamToString(request.body);
+    const data = JSON.parse(body);
+    const username = data.result.user.username;
+    return username;
+}
 export default {
-    image: `https://api.crawlbase.com/screenshots?token=4HrVHd6RoiajGYYEJsx0QA&height=700&url=https://web3.bio/${lastFramerUsername}`,
+    image: `https://api.crawlbase.com/screenshots?token=4HrVHd6RoiajGYYEJsx0QA&height=700&url=https://web3.bio/${username}`,
     buttons: [
         { 
             label: '⬅️ Back',
