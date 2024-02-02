9bfa7074380e42377b5dca18f8bcc512c4429f10
Robert C Edwards <139775+robertcedwards@users.noreply.github.com>
2024-02-01 19:57:17 -0600

    d

--- src/frames/count.js
+++ src/frames/count.js
@@ -1,5 +1,5 @@
 export default {
-    image: `https://api.crawlbase.com/screenshots?token=4HrVHd6RoiajGYYEJsx0QA&height=700&url=https://web3.bio/0xhashbrown.eth`,
+    image: `./https://api.crawlbase.com/screenshots?token=4HrVHd6RoiajGYYEJsx0QA&height=700&url=https://web3.bio/0xhashbrown.eth`,
     buttons: [
         { 
             label: '⬅️ Back',
