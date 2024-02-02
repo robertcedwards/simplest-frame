3d600d79cfac3e243945f091ab0cd9534f038fa0
Robert C Edwards <139775+robertcedwards@users.noreply.github.com>
2024-02-01 20:20:18 -0600

    jl

--- src/frames/count.js
+++ src/frames/count.js
@@ -1,5 +1,7 @@
+const username = data.result.user.username;
+
 export default {
-    image: `https://api.crawlbase.com/screenshots?token=4HrVHd6RoiajGYYEJsx0QA&height=700&url=https://web3.bio/0xhashbrown.eth`,
+    image: (username) => `https://api.crawlbase.com/screenshots?token=4HrVHd6RoiajGYYEJsx0QA&height=700&url=https://web3.bio/${username}`,
     buttons: [
         { 
             label: '⬅️ Back',
